b09b9e44c139d9cbb9349b1a5cd6d13477f5912b
backend <serik1995m@gmail.com>
2022-05-17 22:07:23 +0300

    upgrate swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,28 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Swagger dumbel",
+ *     ),
+ *     @OA\Server(
+ *         description="Api localhost",
+ *         url="http://localhost:8000/"
+ *     ),
+ *     @OA\Components(
+ *         @OA\SecurityScheme(
+ *             securityScheme="bearerAuth",
+ *             in="header",
+ *             name="Authorization",
+ *             type="http",
+ *             scheme="bearer",
+ *             bearerFormat="JWT",
+ *         ),
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- resources/views/swagger.blade.php
+++ resources/views/swagger.blade.php
@@ -1,61 +1,22 @@
-<!-- HTML for static distribution bundle build -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Swagger</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('swagger-assets/swagger-ui.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('swagger-assets/favicon-32x32.png') }}" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="{{ asset('swagger-assets/favicon-16x16.png') }}" sizes="16x16"/>
-    <style>
-        html {
-            box-sizing: border-box;
-            overflow: -moz-scrollbars-vertical;
-            overflow-y: scroll;
-        }
-
-        *,
-        *:before,
-        *:after {
-            box-sizing: inherit;
-        }
-
-        body {
-            margin: 0;
-            background: #fafafa;
-        }
-
-        .info, .models {
-            display: none !important;
-        }
-    </style>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content="SwaggerIU"/>
+    <title>SwaggerUI</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css"/>
 </head>
-
 <body>
 <div id="swagger-ui"></div>
-
-<script src="{{ asset('swagger-assets/swagger-ui-bundle.js') }}"></script>
-<script src="{{ asset('swagger-assets/swagger-ui-standalone-preset.js') }}"></script>
+<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
 <script>
-  window.onload = function () {
-    // Begin Swagger UI call region
-    const ui = SwaggerUIBundle({
-      url: window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/swagger-assets/openapi.yaml",
+  window.onload = () => {
+    window.ui = SwaggerUIBundle({
+      url: "/openapi.yaml",
       dom_id: '#swagger-ui',
-      deepLinking: true,
-      presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIStandalonePreset
-      ],
-      plugins: [
-        SwaggerUIBundle.plugins.DownloadUrl
-      ],
-      layout: "StandaloneLayout"
-    })
-    // End Swagger UI call region
-
-    window.ui = ui
-  }
+    });
+  };
 </script>
 </body>
 </html>
