1fa9ac1091adaecaa939eefe4f9bf9c230acb9fd
backend <serik1995m@gmail.com>
2022-05-22 18:41:37 +0300

    qr code relation for created user

--- app/Services/Api/UserService.php
+++ app/Services/Api/UserService.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace App\Services\Api;
 
+use App\Models\QrCode;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserService
 {
     public function create(array $data): User
     {
+        DB::beginTransaction();
         $user = new User($data);
         $user->save();
+        QrCode::query()->where('uuid', $data['uuid'])->whereNull('user_id')->update(['user_id' => $user->id]);
+        DB::commit();
 
         return $user;
     }
