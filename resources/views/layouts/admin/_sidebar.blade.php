87116fc6fe999dba5a8a69e7aaa01fa4f1810533
s.melnyk <serik1995m@gmail.com>
2023-03-13 22:58:13 +0200

    subscription to gym membership

--- resources/views/layouts/admin/_sidebar.blade.php
+++ resources/views/layouts/admin/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('dashboard') }}" class="brand-link">
-        <img src="img/logo.png" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
+        <img src="/img/logo.png" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
